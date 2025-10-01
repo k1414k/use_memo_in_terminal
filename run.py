aa7afcec2513d1a6d8b551666017f1a29d3838c4
k1414k <k14146k@naver.com>
2025-10-01 22:12:20 +0900

    機能要素追加

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class MemoManager:
         with open(self.LOG_FILE, 'w', encoding='utf-8') as f:
             json.dump(self.memo, f, ensure_ascii=False, indent=2)
 
-    def save(self, text):
+    def write(self, text):
         today = str(datetime.datetime.now().strftime("%Y%m%d"))
 
         if today not in self.memo:
@@ -33,28 +33,64 @@ class MemoManager:
         self._save_log()
         print("メモを保存しました")
 
-    def read(self):
+    def read(self, read_mode):
         if not self.memo:
             print("メモが存在しません")
             return
         
-        latest_date = max(int(k) for k in self.memo.keys()) #一番最新の日付取得
-        for text in self.memo[str(latest_date)]:
-            print(f"({latest_date}) {text}")
+        if read_mode == 1:
+            for k in self.memo.keys():
+                print("---------------------")
+                print(k)
+                for idx,val in enumerate(self.memo[str(k)]):
+                    print(f"({idx}) {val}")
+                print("")
+        else:
+            latest_date = max(int(k) for k in self.memo.keys()) #一番最新の日付取得
+            print(latest_date)
+            for idx,val in enumerate(self.memo[str(latest_date)]):
+                print(f"({idx}) {val}")
+    def edit(self, date, idx, text):
+        print(f"{date},{idx},{text}")
+
+    def delete(self, date, idx):
+        print(self.memo[date][int(idx)])
+        
+
+    def explanation(self):
+        print("""
+    -w: inputされた内容を日付で保存
+    -r :最新内容確認
+    -a: すべての内容確認（最新順降ろす）
+    -e: inputで日付と番号と内容を指定し修正
+    -d: inputで日付と番号を指定し削除
+""")
+        
 
 
 if __name__ == '__main__':
-    mode = 'read'
     manager = MemoManager()
 
     if len(sys.argv) > 1:
-        mode = sys.argv[1].lower()
+        argument = sys.argv[1].lower()
+    else:
+        argument = ""
 
-    if mode == 'save':
-        new_memo = input("新しいメモ作成：  ")
-        manager.save(new_memo)
-    elif mode == 'read':
-        manager.read()
+    if argument == '-w':
+        new_memo = input("新しいメモ作成: ")
+        manager.write(new_memo)
+    elif argument == '-r':
+        manager.read(0)
+    elif argument == '-a':
+        manager.read(1)
+    elif argument == '-e':
+        date = input("修正する日付: ")
+        idx = input("インデックス番号: ")
+        new_memo = input("新しいメモ作成: ")
+        manager.edit(date, idx, new_memo)
+    elif argument == '-d':
+        date = input("削除する日付: ")
+        idx = input("インデックス番号: ")
+        manager.delete(date, idx)
     else: 
-        print("引数が間違っています")
-
+        manager.explanation()
